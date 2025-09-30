3079d316d82b6b35c59563a35f886a2960b568bc
alexmf98 <montiel.20598@gmail.com>
2025-09-30 17:11:17 +0200

    scratch

---
+++ scratch_2.kts
@@ -0,0 +1,11 @@
+//3. Concatenación de Cadenas Crea una función llamada saludo que reciba dos
+//parámetros: un String para el nombre y un Int para la edad. La función debe devolver un
+//String con el formato: "Hola, me llamo [nombre] y tengo [edad] años.". Llama a la función
+//desde el main e imprime su resultado.
+
+fun saludo(nombre: String, edad: Int){
+    print("Hola, me llamo $nombre y tengo $edad años.")
+}
+
+
+saludo("Pepito", 12)
