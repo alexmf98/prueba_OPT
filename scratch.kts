1b7345b5e9ee89d06e174d65ef5562184b61644f
alexmf98 <montiel.20598@gmail.com>
2025-09-30 16:58:30 +0200

    scratch

---
+++ scratch.kts
@@ -0,0 +1,8 @@
+//1. Declaración de Variables Crea un programa que declare cinco variables de diferentes
+//tipos (Int, String, Boolean, Double, Char). Asigna un valor a cada una y luego
+//imprímelas por consola.
+var numero: Int = 12
+val nombre: String = "Juan"
+var booleano: Boolean = true
+var doub: Double = 12.00
+var c: Char = 'a'
---
+++ scratch_1.kts
@@ -0,0 +1,13 @@
+//2. Operaciones Aritméticas Básicas Escribe un programa que declare dos variables
+// numéricas (Int) y realice las cinco operaciones aritméticas básicas con ellas: suma, resta,
+//multiplicación, división y módulo. Imprime el resultado de cada operación con un texto
+//descriptivo (ej. "La suma es: X").
+
+val num1: Int = 2
+val num2: Int = 4
+
+println("La suma es: " + (num1 + num2))
+println("La resta es: " + (num2 - num1))
+println("La multiplicación es " + (num1 * num2))
+println("La division es: " + (num2 / num1))
+print("El modulo da: " + (num2 % num1))
